94ee7bd91b9a4d0a638ac8815a4e6721aab735b6
Na Liu <nliu1@mdanderson.org>
2019-05-15 08:54:09 -0500

    First Commit

---
+++ server.R
@@ -0,0 +1,148 @@
+#rm(list=ls(all=TRUE))
+
+#### install the necessary libraries ===========================================
+list.of.packages <- c("ggplot2", "survival","boot","DT","shiny","shinyjs","cowplot","ggpubr","gridExtra","devtools")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages,library,character.only = TRUE)
+devtools::install_github("NaLiuStat/IPDfromKM")
+
+#### the server function ========================================================
+function(input, output, session) {
+  
+  ## input date file --------------------------------------- 
+  mydata <- eventReactive(input$go,{
+    infile <- isolate(input$file1)
+    if (is.null(infile)) {return(NULL)}
+    read.csv(isolate(input$file1$datapath),header = T,sep = input$sep)
+  })
+  
+  ## input the time intervals -------------------------------
+  trisk <- eventReactive(input$go,{
+    v1 <- as.numeric(unlist(strsplit(isolate(input$trisk),",")))
+    if (is.na(v1)) {return(NULL)}
+    else return(v1)
+  })
+  
+  ## input the numbers of patients at risk -------------------
+  nrisk <- eventReactive(input$go,{
+    v2 <- as.numeric(unlist(strsplit(isolate(input$nrisk),",")))
+    if (is.na(v2)) {return(NULL)}
+    else return(v2)
+  })
+  
+  ## input the scale of suvival rates --------------------------
+  maxy <- eventReactive(input$go,{
+    x1 <- as.numeric(isolate(input$maxy))
+    return(x1)
+  })
+  
+  ## assign the treatment arm indicators ------------------------
+  armind <- eventReactive(input$go,{
+    return(as.numeric(isolate(input$armind)))
+  })
+  
+  ## input the initial number of patients ------------------------
+  totalpts <- eventReactive(input$go,{
+    x2 <- as.numeric(isolate(input$totalpts))
+    if (is.na(x2)) {return(NULL)}
+    else return(x2)
+  })
+  
+  ## input the total number of events if available ----------------
+  totalevent <- eventReactive(input$go,{
+    x3 <- as.numeric(isolate(input$totalevent))
+    if (is.na(x3)) {return(NULL)}
+    else return(x3)
+  })
+  
+  ## preprocess the input data file ---------------------------------
+  pre <- eventReactive(input$go,{
+    preprocess(dat=mydata(),trisk=trisk(),nrisk=nrisk(),totalpts = totalpts(),maxy=maxy())
+  })
+  
+  ## the reconstructed IPD -------------------------------------------
+  mylist <- eventReactive(input$go,{
+    getIPD(prep=pre(),armind=armind(),tot.events=totalevent())
+  })
+  
+  ## take the reset buttion reaction for next curve--------------------
+  observeEvent(input$resetAll, {
+    reset("form")
+  })
+  
+  
+  ## output the CSV file -------------------------------------------------
+  
+  output$downloadData <- downloadHandler(
+    filename = "reconstructIPD.csv",
+    content = function(file) {
+      write.csv(mylist()[[1]], file, row.names = FALSE)
+    }
+  )
+  
+  ## print the CSV file  --------------------------------------------------
+  output$table1 <- renderDataTable(round(mylist()[[1]],3))
+  
+  ## graph plot1: patients number at risk -----------------------------------
+  output$plot1 <- renderPlot({
+    plotrisk <- plotIPD(est=mylist(),ori_dat=mydata(),maxy=maxy())
+    plotrisk
+  },height=400)
+  
+  ## print summary table 1 --------------------------------------------------
+  output$summary1 <- DT::renderDataTable(mylist()[[8]], options=list(
+    columnDefs = list(list(className = 'dt-center', targets = 2)),
+    dom="t",
+    lengthMenu = c(20, 10)
+  ))
+  
+  ## graph plot2: survival plots ---------------------------------------------
+  output$plot2 <- renderPlot({
+    survreport(ipd1=mylist()[[1]],arms=1,interval=6)
+  },height=400)
+  
+  ## print summary table 2 ----------------------------------------------------
+  qt1 <- reactive({
+    fit1 <- survfit(Surv(time,status)~1,data=mylist()[[1]])
+    qt <- data.frame(q = c(.75, .5, .25),
+                     qt = quantile(fit1))
+    qt <- round(qt,4)
+    names(qt) <- c('Survival Rate', 'Time','0.95LCI','0.95UCI')
+    return (qt)
+  })
+  
+  output$summary2 <- DT::renderDataTable(round(qt1(), 3),
+                                         rownames=FALSE,                                     
+                                         options=list(dom="t")
+  )
+  ## out put the report ------------------------------------------------------
+  output$report <- downloadHandler(
+    
+    filename = function() {
+      paste('my-report', sep = '.', switch(
+        input$format, PDF = 'pdf', HTML = 'html', Word = 'docx'
+      ))
+    },
+    
+    content = function(file) {
+      src <- normalizePath('kmreport.Rmd')
+      ## temporarily switch to the temp dir, in case you do not have write
+      ## permission to the current working directory
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      file.copy(src, 'kmreport.Rmd', overwrite = TRUE)
+      
+      ## render report from markdown file
+      library(rmarkdown)
+      out <- render('kmreport.Rmd', switch(
+        input$format,
+        PDF = pdf_document(), HTML = html_document(), Word = word_document()
+      ))
+      file.rename(out, file)
+    }
+  ) 
+  
+  
+}
+
---
+++ ui.R
@@ -0,0 +1,116 @@
+#rm(list=ls(all=TRUE))
+## install the necessary package ==================================================
+list.of.packages <- c("ggplot2", "survival","boot","DT","shiny","shinyjs","ggpubr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages,library,character.only = TRUE)
+
+## start the page arrangement =====================================================
+fluidPage(
+  
+  ## title ------------------------------------------------------------------
+  div(style="color:darkblue",
+      align = "center",
+      headerPanel("Reconstruct Individual Patient Data (IPD) From Kaplan-Meier Survival Curve")
+  ),
+  
+  
+  #titlePanel("Reconstruct Individual Patient Data (IPD) From Kaplan-Meier Survival Curve"),
+  tags$p(tags$h4(("Na Liu, J. Jack Lee"),align="center")),
+  tags$p(tags$h5(tags$em("Department of Biostatistics, MD Anderson Cancer Center"),align="center")),
+  tags$p(tags$h5(tags$em("Last Updat: 04/12/2019"),align="center")),
+  ## sidebar -----------------------------------------------------------------
+  sidebarLayout(
+    sidebarPanel(
+      useShinyjs(),
+      div(
+        tags$style(type = "text/css",
+                   "label { font-size: 12px; }"
+        ),
+        id = "form",
+        # action buttons ----------------------------------
+        wellPanel(
+          tags$p(tags$h4(tags$strong('Calculating Control',style = "color:darkblue;"))),
+          fluidRow(
+            actionButton(inputId = "go",label = "Begin Calculation"),
+            actionButton(inputId ="resetAll", label = "Reset To Default")
+          )),
+        # Input the two-column coordinates file ------
+        wellPanel(
+          tags$p(tags$h4(tags$strong('Choose Data File',style = "color:darkblue;"))),
+          fileInput("file1", "Load the coordinates dataset of survival curve extracted by digitize softwares.
+                    Accept .csv or .txt files.",
+                    multiple = FALSE,
+                    accept = c("text/csv",
+                               "text/comma-separated-values,text/plain",
+                               ".csv")),
+          radioButtons("sep","Separator", inline = T,
+                       choices = c(Comma = ",",
+                                   Tab = "\t"),
+                       selected = ",")
+        ),
+        # other inputs ---------------------
+        wellPanel(
+          # reported times at risk --------
+          tags$p(tags$h4(tags$strong('Reported Times at Risk',style = "color:darkblue;"))),
+          textInput('trisk', 'Separate numbers by commas. 
+                    E.g: 0,6,12,18,24. Can be NULL if not available.','NULL'),
+          # reported patient number at risk ----
+          tags$p(tags$h4(tags$strong('Reported Numbers of Patients at Risk',style = "color:darkblue;"))),
+          textInput('nrisk', 'Separate numbers by commas. 
+                    E.g: 144,83,45,32,10. Can be NULL if not available. ','NULL'),
+          # scale of survival rates -------------
+          tags$p(tags$h4(tags$strong('Scale of Survival Rates',style = "color:darkblue;"))),
+          selectInput("maxy", "If the survival rates are in the form of percentages, use 100;
+                      if in decimals, use 1.", 
+                      c("1","100"),selected = "100"
+          ),
+          # treatment arm indicator ---------------
+          tags$p(tags$h4(tags$strong('Treatment Arm Indicator',style = "color:darkblue;"))),
+          selectInput("armind", "The treatment arm indicator can be an arbitrary number from 0 to 5.",
+                      c("0","1","2","3","4","5"),selected = "0"
+          ),
+          # initial number of patients -------------
+          tags$p(tags$h4(tags$strong('The Initial Number of Patients',style = "color:darkblue;"))),
+          textInput('totalpts', 'Can be NULL when the reported times at risk and patients
+                    numbers at risk are available. Otherwise, an integer number must be input.',
+                    "NULL"
+          ),
+          # total number of events ------------------
+          tags$p(tags$h4(tags$strong('The Initial Number of Patients',style = "color:darkblue;"))),
+          textInput('totalevent', 'Can be NULL if not available.','NULL')
+          ))),
+    ## main panel ----------------------------------------------------------------
+    mainPanel(tabsetPanel(
+      type = "tabs",
+      
+      tabPanel("Plot Estimation", value=2, fluidRow(
+        plotOutput("plot1"),
+        dataTableOutput("summary1")
+      )),
+      tabPanel("Plot Survival", value=3, fluidRow(
+        plotOutput("plot2"),
+        dataTableOutput("summary2")
+      )),
+      tabPanel("Download IPD ",value=4, fluidRow(
+        downloadButton("downloadData", "Download IPD as CSV File"),
+        dataTableOutput('table1')
+      )),
+      tabPanel("Download Report",value=5, fluidRow(
+        radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
+                     inline = TRUE),
+        downloadButton("report","Download Report")
+      )),
+      tabPanel("Help",value=1, 
+               tags$iframe(style="width: 100%; height: 600px; scrolling=yes",
+                           src="KMshinyhelp.pdf")
+      )
+      
+    ))
+        )
+)
+
+
+
+
+
